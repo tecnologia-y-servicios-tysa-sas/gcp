e546516c0b5d30876176707f4c92feec61c6a8fa
Desarrollador <dexterlabcode@gmail.com>
2019-06-18 23:38:19 -0500

    Cambios en la funcionalidad de seguimiento de contratos - Parte 1

---
+++ GCP_CF/Controllers/SeguimientoController.cs
@@ -0,0 +1,186 @@
+﻿using GCP_CF.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using PagedList;
+using System;
+
+namespace GCP_CF.Controllers
+{
+    public class SeguimientoController : Controller
+    {
+        private GCPContext db = new GCPContext();
+
+        // GET: Seguimiento
+        public ViewResult Index(string sortBy, string currentFilter, string searchString, int? page)
+        {
+            var listadoContratos = GetListadoContratos(sortBy, currentFilter, searchString, page);
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(listadoContratos.ToPagedList<Contratos>(pageNumber, pageSize));
+        }
+
+        // GET: FasesContrato
+        public ActionResult FasesContrato(int? idContrato)
+        {
+            if (idContrato == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            Contratos contrato = db.Contratos.Find(idContrato);
+            ViewBag.Contrato = contrato;
+
+            List<Registrofacescontratos> listadoFasesContrato = (from e in db.Registrofacescontratos.Include(a => a.FasesContrato)
+                                                                 where e.Contrato_Id == idContrato
+                                                                 select e).ToList();
+            ViewBag.FasesContrato = listadoFasesContrato;
+
+            return View();
+        }
+
+        public JsonResult ObtenerListadoFasesContrato(int idContrato)
+        {
+            // TODO: Mejorar esta respuesta construyendo objetos
+            object response = null;
+
+            try
+            {
+                var listadoFasesContrato = db.Registrofacescontratos
+                                            .Include(a => a.FasesContrato)
+                                            .Where(e => e.Contrato_Id == idContrato)
+                                            .Select(e => new { e.FasesContrato.fase_Id, e.FasesContrato.Descripcion });
+                if (listadoFasesContrato.Count() > 0)
+                    response = new { fases = listadoFasesContrato };
+                else
+                    response = new { mensaje = "No existen fases asociadas a este contrato" };
+
+            } catch (Exception e)
+            {
+                response = new { mensaje = "Ha ocurrido un error al obtener las fases asociadas a este contrato", detalle = e.Message };
+            }
+
+            return Json(response);
+        }
+
+        [HttpPost]
+        public JsonResult ObtenerFasesNoAsociadas(int idContrato)
+        {
+            List<int> listadoFasesContrato = (from fc in db.Registrofacescontratos.Include(a => a.FasesContrato)
+                                                                 where fc.Contrato_Id == idContrato
+                                                                 select fc.Fase_Id).ToList();
+
+            var listadoFasesNoAsociadas = db.FasesContrato.Select(f => new { f.fase_Id, f.Descripcion }).Where(f => !listadoFasesContrato.Contains(f.fase_Id));
+
+            var response = "{ \"fases\":" + JsonConvert.DeserializeObject(JsonConvert.SerializeObject(listadoFasesNoAsociadas)) + "}";
+            return Json(response);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarFaseContrato(int idContrato, int idFase)
+        {
+            string mensaje = "";
+            string error = "";
+
+            string numeroContrato = db.Contratos.Where(c => c.Contrato_Id == idContrato).Select(c => c.NumeroContrato).FirstOrDefault();
+            string nombreFase = db.FasesContrato.Where(f => f.fase_Id == idFase).Select(f => f.Descripcion).FirstOrDefault();
+
+            Registrofacescontratos rfc = new Registrofacescontratos();
+            rfc.Contrato_Id = idContrato;
+            rfc.Fase_Id = idFase;
+
+                if (rfc.Fase_Id != 0)
+                    if (db.Registrofacescontratos.Where(x => x.Fase_Id == rfc.Fase_Id && x.Contrato_Id == rfc.Contrato_Id).Select(x => x).ToList().Count() == 0)
+                    {
+                        db.Registrofacescontratos.Add(rfc);
+                        db.SaveChanges();
+                    mensaje = "La fase &quot;" + nombreFase.Trim() + "&quot; fue agregada exitosamente al contrato " + numeroContrato + ".";
+                    }
+                    else
+                    {
+                        error = "<b>Error:</b> La fase &quot;" + nombreFase.Trim() + "&quot; ya se encuentra asignada al contrato" + numeroContrato + ".";
+                    }
+
+            return Json("{ \"mensaje\": \"" + mensaje + "\", \"error\": \"" + error + "\"}");
+        }
+
+        private IQueryable<Contratos> GetListadoContratos(string sortBy, string currentFilter, string searchString, int? page)
+        {
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+            ViewBag.SortByIdContrato = sortBy == "idContrato|ASC" ? "idContrato|DESC" : "idContrato|ASC";
+            ViewBag.SortByFechaInicio = sortBy == "fechaInicio|ASC" ? "fechaInicio|DESC" : "fechaInicio|ASC";
+            ViewBag.SortByEntidadContratante = sortBy == "fechaInicio|ASC" ? "entidadContratante|DESC" : "entidadContratante|ASC";
+            ViewBag.SortByObjetoContractual = sortBy == "objetoContractual|ASC" ? "objetoContractual|DESC" : "objetoContractual|ASC";
+            ViewBag.SortByPlazo = sortBy == "objetoContractual|ASC" ? "plazo|DESC" : "plazo|ASC";
+            ViewBag.SortByFechaTerminacion = sortBy == "fechaTerminacion|ASC" ? "fechaTerminacion|DESC" : "fechaTerminacion|ASC";
+            ViewBag.SortByValorContrato = sortBy == "valorContrato|ASC"  ? "valorContrato|DESC" : "valorContrato|ASC";
+
+            ViewBag.CurrentSort = sortBy;
+            ViewBag.CurrentFilter = searchString;
+
+            var listadoContratos = from c in db.Contratos
+                                   select c;
+
+            if (!string.IsNullOrEmpty(searchString))
+                listadoContratos = listadoContratos.Where(c => c.NumeroContrato.Contains(searchString));
+
+            switch (sortBy)
+            {
+                case "idContrato|DESC":
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.NumeroContrato);
+                    break;
+                case "idContrato|ASC":
+                    listadoContratos = listadoContratos.OrderBy(c => c.NumeroContrato);
+                    break;
+                case "fechaInicio|DESC":
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.FechaInicio);
+                    break;
+                case "fechaInicio|ASC":
+                    listadoContratos = listadoContratos.OrderBy(c => c.FechaInicio);
+                    break;
+                case "entidadContratante|DESC":
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.EntidadContratante);
+                    break;
+                case "entidadContratante|ASC":
+                    listadoContratos = listadoContratos.OrderBy(c => c.EntidadContratante);
+                    break;
+                case "objetoContractual|DESC":
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.ObjetoContractual);
+                    break;
+                case "objetoContractual|ASC":
+                    listadoContratos = listadoContratos.OrderBy(c => c.ObjetoContractual);
+                    break;
+                case "plazo|DESC":
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.Plazo);
+                    break;
+                case "plazo|ASC":
+                    listadoContratos = listadoContratos.OrderBy(c => c.Plazo);
+                    break;
+                case "fechaTerminacion|DESC":
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.FechaTerminacion);
+                    break;
+                case "fechaTerminacion|ASC":
+                    listadoContratos = listadoContratos.OrderBy(c => c.FechaTerminacion);
+                    break;
+                case "valorContrato|DESC":
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.ValorContrato);
+                    break;
+                case "valorContrato|ASC":
+                    listadoContratos = listadoContratos.OrderBy(c => c.ValorContrato);
+                    break;
+                default:
+                    listadoContratos = listadoContratos.OrderByDescending(c => c.NumeroContrato);
+                    break;
+            }
+
+            return listadoContratos;
+        }
+    }
+}
---
+++ GCP_CF/Scripts/noGeneric/FasesContrato.js
@@ -0,0 +1,166 @@
+﻿function VerFasesContrato(IdContrato) {
+    console.log("Solicitando fases para el contrato " + IdContrato);
+    $(location).attr("href", "/Seguimiento/FasesContrato?idContrato=" + IdContrato);
+}
+
+function ObtenerListadoFasesContrato(idContrato) {
+
+    var tblListadoFases = $("#tblListadoFases");
+    $(tblListadoFases).html("");
+    $(tblListadoFases).append("<tbody><tr><td class=\"alert-warning\">Cargando fases...</td></tr><tbody>");
+
+    $.ajax({
+        async: false,
+        url: "/Seguimiento/ObtenerListadoFasesContrato",
+        type: "POST",
+        data: JSON.stringify({ idContrato: idContrato }),
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            if (data.fases && data.fases.length > 0) {       
+                $(tblListadoFases).html("");
+                var tblHeader = "<thead><tr>"
+                    + "<th class=\"col-md-1\">ID</th>"
+                    + "<th class=\"col-md-10\">Fase</th>"
+                    + "<th class=\"col-md-1 text-center\">Acciones</th>"
+                    + "</tr></thead>"
+                    + "<tbody>";
+                $("#tblListadoFases").append(tblHeader);
+                var i = 0;
+                $.each(data.fases, function (key, entry) {
+                    i++;
+                    var tblRow = "<tr>"
+                        + "<td class=\"col-md-1\">" + i + "</td>"
+                        + "<td class=\"col-md-10\">" + entry.Descripcion.trim() + "</td>"
+                        + "<td class=\"col-md-1 text-center\">"
+                        + "<a class=\"btn-details\" href=\"javascript:void(0)\" title=\"Ver Actividades\" onclick=\"VerActividadesFase(" + entry.fase_Id + ")\">Actividades</a>"
+                        + "<a class=\"btn-delete\" href=\"javascript:void(0)\" title=\"Eliminar\" onclick=\"EliminarFaseContrato(" + entry.fase_Id + ")\">Eliminar</a>"
+                        + "</td>"
+                        + "</tr>";
+                    $(tblListadoFases).append(tblRow);
+                });
+                $(tblListadoFases).append($("</tbody>"));
+            } else {
+                $(tblListadoFases).html("");
+                $(tblListadoFases).append($("<tbody>"));
+                $(tblListadoFases).append($("<tr><td class=\"alert-info\">" + data.mensaje + "</td></tr>"));
+                $(tblListadoFases).append($("</tbody>"));
+                console.log(data.detalle);
+            }
+        },
+        error: function () {
+            $(tblListadoFases).html("");
+            $(tblListadoFases).append($("<tbody>"));
+            $(tblListadoFases).append($("<tr><td class=\"alert-danger\">Ha ocurrido un error al cargar las fases del contrato</td></tr>"));
+            $(tblListadoFases).append($("</tbody>"));
+        }
+    });
+}
+
+function AgregarFaseContrato(idContrato) {
+    $("#agregarFaseContrato").modal("show");
+    CargarFasesDisponibles(idContrato);
+}
+
+function VerActividadesFase(idContrato) {
+    NoImplementado();
+}
+
+function EliminarFaseContrato(idContrato) {
+    NoImplementado();
+}
+
+function LimpiarModalFaseContrato() {
+    $("#selectGroup").hide();
+    $("#mensajeError").text("");
+    $("#mensajeError").hide();
+    $("#idFaseDisponible option").remove();
+}
+
+function CerrarModalFaseContrato() {
+    LimpiarModalFaseContrato();
+    $("#agregarFaseContrato").modal("hide");
+}
+
+function CargarFasesDisponibles(idContrato) {
+
+    LimpiarModalFaseContrato();
+
+    selectGroup = $("#selectGroup");
+    fasesDisponibles = $("#idFaseDisponible");
+    mensajeError = $("#mensajeError");
+
+    $.ajax({
+        async: false,
+        url: "/Seguimiento/ObtenerFasesNoAsociadas",
+        type: "POST",
+        data: JSON.stringify({ idContrato: idContrato }),
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            jsonData = jQuery.parseJSON(data);
+            if (jsonData.fases.length > 0 && jsonData.fases[0].fase_Id) {
+                $(fasesDisponibles).append($("<option></option>").attr("value", "-1").text("Seleccione..."));
+                $(selectGroup).fadeIn();
+                $.each(jsonData.fases, function (key, entry) {
+                    $(fasesDisponibles).append($("<option></option>").attr("value", entry.fase_Id).text(entry.Descripcion));
+                });
+            } else {
+                console.log("AQUI");
+                $(mensajeError).text("No hay más fases para agregar a este contrato.");
+                $(mensajeError).fadeIn();
+            }
+        }, 
+        error: function () {
+            $(mensajeError).text("Ha ocurrido un error al cargar la lista.");
+            $(mensajeError).fadeIn();
+        }
+    });
+}
+
+function AgregarFaseSeleccionada(idContrato) {
+
+    var mensaje = "";
+    var claseMensaje = "";
+    var idFase = $("#idFaseDisponible option:selected").val();
+    var textoFase = $("#idFaseDisponible option:selected").text();
+
+    $("#mensajeAccion").hide();
+    $("#textoMensaje").removeClass("alert-info").removeClass("alert-warning").text("");
+
+    if (idFase == "" || idFase == "-1") {
+        alert("Por favor seleccione una fase para agregar al contrato");
+        return false;
+    } else {
+        if (confirm("¿Está seguro de agregar la fase " + textoFase.trim() + " al contrato actual?")) {
+            $.ajax({
+                async: false,
+                url: "/Seguimiento/GuardarFaseContrato",
+                type: "POST",
+                data: JSON.stringify({ idContrato: idContrato, idFase: idFase }),
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    jsonData = jQuery.parseJSON(data);
+                    console.log(jsonData);
+                    if (jsonData.error.trim().length > 0) {
+                        mensaje = jsonData.error.trim();
+                        claseMensaje = "alert-danger";
+                    } else {
+                        mensaje = jsonData.mensaje.trim();
+                        claseMensaje = "alert-info";
+                    }
+                }
+            });
+
+            ObtenerListadoFasesContrato(idContrato); // Refrescar el listado
+            CerrarModalFaseContrato();
+            $("#mensajeAccion").addClass(claseMensaje).fadeIn();
+            $("#textoMensaje").html(mensaje);
+        }
+    }
+}
+
+function NoImplementado() {
+    alert("Función no implementada por el momento");
+}
