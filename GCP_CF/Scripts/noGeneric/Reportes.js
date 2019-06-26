73b3ce9fd1395d0a056e0d3d9adb09fc0ceaf9ee
Desarrollador <dexterlabcode@gmail.com>
2019-06-26 01:51:06 -0500

    Adición de filtros de búsqueda y parte de la lógica en el reporte de contratos.

---
+++ GCP_CF/Scripts/noGeneric/Reportes.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $("#filtrosReporteContratosForm").submit(function () {
+        NoDisponible();
+        return false;
+    });
+});
+
+function NoDisponible() {
+    alert("Por ahora esta funcionalidad no se encuentra disponible.");
+}
