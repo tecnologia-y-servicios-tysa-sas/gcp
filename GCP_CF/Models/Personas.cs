4aa829e15d7e9653028fa906895744ce9bd8f1d5
Gustavo Salgado <tavosalgado@hotmail.com>
2019-06-28 06:39:31 -0500

    Se agrega validacion de Email

--- GCP_CF/Models/Personas.cs
+++ GCP_CF/Models/Personas.cs
@@ -13,11 +13,18 @@ namespace GCP_CF.Models
         public string Apellidos { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Correo {0} no puede ser más largo de {1} caracteres.")]
+        [RegularExpression(@"\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b", ErrorMessage = "Correo electrónico no es valido")]
         public string Correo { get; set; }
+
         [Display(Name = "Número de Identificación")]
         public string NumeroDocumento { get; set; }
+
         public Nullable<int> TipoPersona_Id { get; set; }
+
         public int? TipoDocumentoId { get; set; }
+
         [NotMapped]
         [Display(Name = "Nombre Completo")]
         public String NombreCompleto { get { return Nombres + ' ' + Apellidos; } }
