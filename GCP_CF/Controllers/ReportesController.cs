e79dc2738fff608543ae695820d326c5ea1381a6
Desarrollador <dexterlabcode@gmail.com>
2019-06-28 00:50:37 -0500

    Implementación para mostrar resultados de la consulta del reporte de contratos

--- GCP_CF/Controllers/ReportesController.cs
+++ GCP_CF/Controllers/ReportesController.cs
@@ -12,10 +12,16 @@ namespace GCP_CF.Controllers
     {
         private GCPContext db = new GCPContext();
 
-        public ActionResult Contratos(int? Anio, int? IdEntidadContratante, string NumeroContrato, int? IdEstadoContrato)
+        public ActionResult Contratos(FormCollection filterForm)
         {
-            ViewBag.IdEstadoContrato = new SelectList(db.TiposEstadoContrato.OrderBy(x => x.Descripcion), "TiposEstadoContrato_Id", "Descripcion", IdEstadoContrato.GetValueOrDefault());
-            ViewBag.IdEntidadContratante = new SelectList(db.Personas.Where(x => x.TipoPersona_Id == 3), "Persona_Id", "NombreCompleto", IdEntidadContratante.GetValueOrDefault());
+            int anio = !string.IsNullOrEmpty(filterForm["Anio"]) ? int.Parse(filterForm["Anio"]) : 0;
+            int idEntidadContratante = !string.IsNullOrEmpty(filterForm["IdEntidadContratante"]) ? int.Parse(filterForm["IdEntidadContratante"]) : 0;
+            string numeroContrato = filterForm["NumeroContrato"];
+            int idEstadoContrato = !string.IsNullOrEmpty(filterForm["IdEstadoContrato"]) ? int.Parse(filterForm["IdEstadoContrato"]) : 0;
+
+            ViewBag.IdEstadoContrato = new SelectList(db.TiposEstadoContrato.OrderBy(x => x.Descripcion), "TiposEstadoContrato_Id", "Descripcion", idEstadoContrato);
+            ViewBag.IdEntidadContratante = new SelectList(db.Personas.Where(x => x.TipoPersona_Id == 3), "Persona_Id", "NombreCompleto", idEntidadContratante);
+            ViewBag.NumeroContrato = numeroContrato;
 
             HashSet<int> aniosContratos = new HashSet<int>();
             var fechasContratos = db.Contratos.Select(c => new { c.FechaInicio, c.FechaTerminacion, c.FechaActaInicio, c.FechaCdp, c.FechaCrp });
@@ -28,10 +34,10 @@ namespace GCP_CF.Controllers
                 aniosContratos.Add(fechaContrato.FechaCrp.GetValueOrDefault().Year);
             }
 
-            ViewBag.Anio = new SelectList(aniosContratos.Distinct().Where(x => x > 0).OrderByDescending(x => x), Anio.GetValueOrDefault());
+            ViewBag.Anio = new SelectList(aniosContratos.Distinct().Where(x => x > 0).OrderByDescending(x => x), anio);
 
-            List<Contratos> list = ObtenerContratos(Anio, IdEntidadContratante, NumeroContrato, IdEstadoContrato);
-            return View(list.OrderBy(x => x.Contrato_Id).ToList());
+            List<Contratos> list = ObtenerContratos(anio, idEntidadContratante, numeroContrato, idEstadoContrato);
+            return View(list != null ? list.OrderBy(x => x.Contrato_Id).ToList() : new List<Contratos>());
         }
 
         public ActionResult Facturas()
@@ -39,48 +45,52 @@ namespace GCP_CF.Controllers
             return View();
         }
 
-        private List<Contratos> ObtenerContratos(int? anio, int? idEntidadContratante, string numeroContrato, int? idEstadoContrato)
+        private List<Contratos> ObtenerContratos(int anio, int idEntidadContratante, string numeroContrato, int idEstadoContrato)
         {
-            DateTime? fechaInicio = anio.HasValue ? new DateTime(anio.Value, 1, 1) : new DateTime?();
-            DateTime? fechaFin = anio.HasValue ? new DateTime(anio.Value, 12, 1) : new DateTime?();
+            DateTime? fechaInicio = anio > 0 ? new DateTime(anio, 1, 1) : new DateTime?();
+            DateTime? fechaFin = anio > 0 ? new DateTime(anio, 12, 31) : new DateTime?();
 
-            List<Contratos> list1 = new List<Contratos>();
-            if (fechaInicio.HasValue && fechaFin.HasValue)
+            bool hayFechas = fechaInicio.HasValue && fechaFin.HasValue;
+            bool hayEntidadContratante = idEntidadContratante > 0;
+            bool hayNumeroContrato = !string.IsNullOrEmpty(numeroContrato);
+            bool hayEstadoContrato = idEstadoContrato > 0;
+
+            List<Contratos> list = null;
+
+            if (hayFechas || hayEntidadContratante || hayNumeroContrato || hayEstadoContrato)
             {
-                ViewBag.Anio = anio.Value;
-                list1 = db.Contratos.Include(c => c.HistoriaObservaciones)
-                                    .Where(c => (fechaInicio.Value <= c.FechaInicio && c.FechaInicio <= fechaFin)
-                                            || (fechaInicio.Value <= c.FechaTerminacion && c.FechaInicio <= fechaFin)
-                                            || (fechaInicio.Value <= c.FechaActaInicio && c.FechaInicio <= fechaFin)
-                                            || (fechaInicio.Value <= c.FechaCdp && c.FechaInicio <= fechaFin)
-                                            || (fechaInicio.Value <= c.FechaCrp && c.FechaInicio <= fechaFin)).ToList<Contratos>();
+                list = (from c in db.Contratos
+                         where (
+                             (
+                                 hayFechas ?
+                                     (fechaInicio.Value <= c.FechaInicio && c.FechaInicio <= fechaFin.Value) ||
+                                     (fechaInicio.Value <= c.FechaTerminacion && c.FechaInicio <= fechaFin.Value) ||
+                                     (fechaInicio.Value <= c.FechaActaInicio && c.FechaInicio <= fechaFin.Value) ||
+                                     (fechaInicio.Value <= c.FechaCdp && c.FechaInicio <= fechaFin.Value) ||
+                                     (fechaInicio.Value <= c.FechaCrp && c.FechaInicio <= fechaFin.Value)
+                                 : true
+                             )
+                             && (hayEntidadContratante ? c.EntidadContratante.Persona_Id == idEntidadContratante : true)
+                             && (hayNumeroContrato ? c.NumeroContrato.Contains(numeroContrato) : true)
+                             && (hayEstadoContrato ? c.TipoEstadoContrato_Id == idEstadoContrato : true)
+                        )
+                        select c).Include(c => c.HistoriaObservaciones).ToList<Contratos>();
             }
 
-            List<Contratos> list2 = new List<Contratos>();
-            if (idEntidadContratante.HasValue)
+            if (list != null)
             {
-                ViewBag.IdEntidadContratante = idEntidadContratante.Value;
-                list2 = db.Contratos.Include(c => c.HistoriaObservaciones)
-                                    .Where(c => c.EntidadContratante.Persona_Id == idEntidadContratante.Value).ToList<Contratos>();
+                var estados = db.TiposEstadoContrato.ToList();
+                foreach (Contratos item in list)
+                {
+                    foreach (var estado in estados)
+                    {
+                        if (item.TipoEstadoContrato_Id == estado.TiposEstadoContrato_Id)
+                        {
+                            item.Estado = estado.Descripcion;
+                        }
+                    }
+                }
             }
-
-            List<Contratos> list3 = new List<Contratos>();
-            if (numeroContrato != "")
-            {
-                ViewBag.NumeroContrato = numeroContrato;
-                list3 = db.Contratos.Include(c => c.HistoriaObservaciones)
-                                    .Where(c => c.NumeroContrato == numeroContrato).ToList<Contratos>();
-            }
-
-            List<Contratos> list4 = new List<Contratos>();
-            if (idEstadoContrato.HasValue)
-            {
-                ViewBag.IdEstadoContrato = idEstadoContrato.Value;
-                list4 = db.Contratos.Include(c => c.HistoriaObservaciones)
-                                    .Where(c => c.TipoEstadoContrato_Id == idEstadoContrato).ToList<Contratos>();
-            }
-
-            List<Contratos> list = list1.Intersect(list2.Intersect(list3.Intersect(list4))).ToList<Contratos>();
 
             return list;
         }
--- GCP_CF/Scripts/noGeneric/Reportes.js
+++ GCP_CF/Scripts/noGeneric/Reportes.js
@@ -1,10 +1,30 @@
-﻿$(function () {
-    $("#filtrosReporteContratosForm").submit(function () {
-        NoDisponible();
+﻿function validarAnio() {
+    if ($('#anioContrato').val() == "") {
+        $('#anioContrato').css('border-color', 'red');
+        $("#mensaje").html('<div><strong>¡Error!</strong> Debe seleccionar un año</div>');
+        $("#modalValidacion").modal('show');
+        setTimeout(function () {
+            $('#modalValidacion').modal('hide');
+        }, 5000);
         return false;
+    }
+}
+
+$(function () {
+
+    $("#anioContrato").change(function () {
+        $("#anioContrato").css('border-color', $("#anioContrato").val() == "" ? 'red' : "green");
+    });
+
+    $("#filtrosReporteContratosForm").submit(function () {
+        return validarAnio();
     });
 });
 
+function ExportarContratos() {
+    NoDisponible();
+}
+
 function NoDisponible() {
-    alert("Por ahora esta funcionalidad no se encuentra disponible.");
+    alert("Por el momento esta funcionalidad no se encuentra disponible.");
 }
