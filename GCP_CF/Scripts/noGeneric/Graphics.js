417e6de0bbe55e9d70cfb881b3863aa0c6aa6c47
Desarrollador <dexterlabcode@gmail.com>
2019-07-10 22:12:39 -0500

    Adición de gráfico de total de presupuesto y otros ajustes

---
+++ GCP_CF/Scripts/noGeneric/Graphics.js
@@ -0,0 +1,15 @@
+﻿function CargarReporte(width, height, target, msgTarget) {
+
+    var textoFallido = "No se pudo cargar la imagen";
+    var url = "/Graphics/PresupuestoPorAnio";
+
+    $('<img src="' + url + '">').on("load", function () {
+        $(msgTarget).fadeOut();
+        $(this).width(width).height(height).appendTo(target);
+        $(target).fadeIn();
+    });
+}
+
+$(document).ready(function () {
+    CargarReporte(600, 400, "#imgReportePresupuesto", "#msgReportePresupuesto");
+})
