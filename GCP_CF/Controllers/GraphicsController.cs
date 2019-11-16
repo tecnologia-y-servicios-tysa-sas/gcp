57eb8c2edcc2e07d474a10f2c3326f1b0d77a9db
John Edwar Escobar <johned11@hotmail.com>
2019-11-16 17:41:12 -0500

    Graficas

--- GCP_CF/Controllers/GraphicsController.cs
+++ GCP_CF/Controllers/GraphicsController.cs
@@ -126,7 +126,7 @@ namespace GCP_CF.Controllers
             return Json(JsonConvert.SerializeObject(chartData, _jsonSetting));
         }
 
-        [HttpPost]
+        /*[HttpPost]
         public JsonResult PresupuestoPorMunicipio(FormCollection filterForm)
         {
             int anioActual = DateTime.Now.Year;
@@ -139,34 +139,36 @@ namespace GCP_CF.Controllers
             Dictionary<string, List<double>> valoresContratoMunicipio = new Dictionary<string, List<double>>();
             Dictionary<string, List<double>> valoresRecursosMunicipio = new Dictionary<string, List<double>>();
 
-            // TODO: Agrupar por el municipio!!!
-            for (int anio = anioActual - 2; anio <= anioActual; anio++)
+            // TODO: Agrupar por el municipio!!
+            foreach (Personas municipio in municipios)
             {
-                foreach (Personas municipio in municipios)
+                double valorContratos = 0;
+                double valorRecursos = 0;
+                string labelActualContratos = string.Empty;
+                string labelActualRecursos = string.Empty;
+                for (int anio = anioActual - 2; anio <= anioActual; anio++)
                 {
-                    string labelActualContratos = municipio.NombreCompleto.ToUpper() + " - " + VALOR_CONTRATOS;
-                    string labelActualRecursos = municipio.NombreCompleto.ToUpper() + " - " + RECURSOS_ADMINISTRADOS;
+                    labelActualContratos = municipio.NombreCompleto.ToUpper() + " - " + VALOR_CONTRATOS;
+                    labelActualRecursos = municipio.NombreCompleto.ToUpper() + " - " + RECURSOS_ADMINISTRADOS;
                     if (!valoresContratoMunicipio.ContainsKey(labelActualContratos)) valoresContratoMunicipio.Add(labelActualContratos, new List<double>());
                     if (!valoresRecursosMunicipio.ContainsKey(labelActualRecursos)) valoresRecursosMunicipio.Add(labelActualRecursos, new List<double>());
 
                     try
                     {
-                        double valorContratos = db.Contratos.Where(c => c.Persona_Id == municipio.Persona_Id && c.FechaInicio.Year >= anio && c.FechaTerminacion.Year <= anio)
+                        valorContratos = db.Contratos.Where(c => c.Persona_Id == municipio.Persona_Id && c.FechaInicio.Year >= anio && c.FechaTerminacion.Year <= anio)
                             .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? c.ValorContrato : 0).DefaultIfEmpty(0).Sum();
 
-                        double valorRecursos = db.Contratos.Where(c => c.Persona_Id == municipio.Persona_Id && c.FechaInicio.Year >= anio && c.FechaTerminacion.Year <= anio)
+                        valorRecursos = db.Contratos.Where(c => c.Persona_Id == municipio.Persona_Id && c.FechaInicio.Year >= anio && c.FechaTerminacion.Year <= anio)
                             .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? c.ValorAdministrar : 0).DefaultIfEmpty(0).Sum();
-
-                        valoresContratoMunicipio[labelActualContratos].Add(valorContratos);
-                        valoresRecursosMunicipio[labelActualRecursos].Add(valorRecursos);
                     }
                     catch (Exception)
                     {
                         chartData.datasets.Add(new ChartDataSet { label = "Error", data = new List<double>() });
-                    }
+                    }                    
                 }
-
-                chartData.labels.Add(anio.ToString());
+                valoresContratoMunicipio[labelActualContratos].Add(valorContratos);
+                valoresRecursosMunicipio[labelActualRecursos].Add(valorRecursos);
+                chartData.labels.Add(municipio.NombreCompleto);
             }
 
             // Datasets
@@ -181,6 +183,126 @@ namespace GCP_CF.Controllers
                 valoresRecursosMunicipio.TryGetValue(lblItem, out List<double> listaRecursosMunicipio);
                 chartData.datasets.Add(new ChartDataSet { label = lblItem, data = listaRecursosMunicipio });
             }
+
+            return Json(JsonConvert.SerializeObject(chartData, _jsonSetting));
+        }*/
+
+        public JsonResult PresupuestoPorMunicipio(int? anioSeleccionado, int? idEntidadContratante, string numeroContrato, int? idEstadoContrato)
+        {
+            int anioActual = DateTime.Now.Year;
+
+            List<Contratos> contratos = db.Contratos.ToList();
+
+            ChartData chartData = new ChartData();
+            chartData.Initialize();
+
+            if (idEntidadContratante.HasValue && idEntidadContratante.Value != -1)
+                contratos = contratos.Where(c => c.Persona_Id == idEntidadContratante).ToList();
+
+            if (!string.IsNullOrEmpty(numeroContrato))
+            {
+                int idContrato = (from cm in db.Contratos
+                                  where cm.NumeroContrato.ToUpper().Trim() == numeroContrato.ToUpper().Trim()
+                                  select cm.Contrato_Id).FirstOrDefault();
+
+                contratos = contratos.Where(c => (idContrato > 0 ?
+                            c.Contrato_Id == idContrato || (c.ContratoMarco_Id.HasValue && c.ContratoMarco_Id.Value == idContrato) : true))
+                            .ToList();
+            }
+
+            if (idEstadoContrato.HasValue && idEstadoContrato.Value != -1)
+                contratos = contratos.Where(c => c.TipoEstadoContrato_Id == idEstadoContrato).ToList();
+
+            int limiteAnios = 0;
+            int anioSuperior = anioActual;
+            if (!anioSeleccionado.HasValue || anioSeleccionado.Value <= 0)
+            {
+                limiteAnios = 3;
+            }
+            else
+            {
+                if (anioActual > anioSeleccionado.Value)
+                    limiteAnios = anioActual - anioSeleccionado.Value;
+                else
+                {
+                    anioSuperior = anioSeleccionado.Value;
+                    limiteAnios = 0;
+                }
+            }
+
+            if (limiteAnios > 3) limiteAnios = 3;
+            int anioInferior = anioActual - limiteAnios;
+
+            List<Personas> municipios = new List<Personas>();
+
+            foreach (var contrato in contratos.Select(c=> c.Persona_Id).Distinct().ToList())
+            {
+                municipios.Add(db.Personas.Where(p => p.TipoPersona_Id == tipoEntidadContratante && p.Persona_Id == contrato).FirstOrDefault());
+            }
+            //List<Personas> municipios = db.Personas.Where(p => p.TipoPersona_Id == tipoEntidadContratante).ToList();            
+
+            Dictionary<string, List<double>> valoresContratoMunicipio = new Dictionary<string, List<double>>();
+            Dictionary<string, List<double>> valoresRecursosMunicipio = new Dictionary<string, List<double>>();
+            List<double> valoresContratos = new List<double>();
+            List<double> valoresResursos = new List<double>();
+            List<double> valoresParticipacion = new List<double>();
+            // TODO: Agrupar por el municipio!!
+            foreach (Personas municipio in municipios)
+            {
+                double valorContratos = 0;
+                double valorRecursos = 0;
+                double valorParticipaciones = 0;
+                string labelActualContratos = string.Empty;
+                string labelActualRecursos = string.Empty;
+                labelActualContratos = VALOR_CONTRATOS;
+                labelActualRecursos = RECURSOS_ADMINISTRADOS;
+                for (int anio = anioInferior; anio <= anioSuperior; anio++)
+                {
+                    try
+                    {
+                        double valorContrato = db.Contratos.Where(c => c.Persona_Id == municipio.Persona_Id && c.FechaInicio.Year == anio && c.FechaTerminacion.Year == anio)
+                            .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? c.ValorContrato : 0).DefaultIfEmpty(0).Sum();
+                        valorContratos += valorContrato;
+
+                        double valorRecurso = db.Contratos.Where(c => c.Persona_Id == municipio.Persona_Id && c.FechaInicio.Year == anio && c.FechaTerminacion.Year == anio)
+                            .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? c.ValorAdministrar : 0).DefaultIfEmpty(0).Sum();
+                        valorRecursos += valorRecurso;
+
+                        double valorParticipacion = db.Contratos.Where(c => c.Persona_Id == municipio.Persona_Id && c.FechaInicio.Year == anio && c.FechaTerminacion.Year == anio)
+                            .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? c.ValorAdministrar : 0).DefaultIfEmpty(0).Sum();
+                        valorParticipaciones += valorParticipacion;
+                    }
+                    catch (Exception)
+                    {
+                        chartData.datasets.Add(new ChartDataSet { label = "Error", data = new List<double>() });
+                    }
+                }
+                valoresContratos.Add(valorContratos);
+                valoresResursos.Add(valorRecursos);
+                valoresParticipacion.Add(valorParticipaciones);
+                chartData.labels.Add(municipio.NombreCompleto);
+            }
+
+            for (int i = 0; i < valoresParticipacion.Count; i++)
+            {
+                valoresParticipacion[i] = valoresParticipacion[i] / valoresContratos.Sum();
+            }
+
+            chartData.datasets.Add(new ChartDataSet { label = "VALOR_CONTRATOS", data = valoresContratos });
+            chartData.datasets.Add(new ChartDataSet { label = "RECURSOS_ADMINISTRADOS", data = valoresResursos });
+
+            // Datasets
+            //foreach (string lblItem in valoresContratoMunicipio.Select(v => v.Key))
+            //{
+            //    valoresContratoMunicipio.TryGetValue(lblItem, out List<double> listaValoresContratos);
+            //    chartData.datasets.Add(new ChartDataSet { label = lblItem, data = listaValoresContratos });
+            //}
+
+            //foreach (string lblItem in valoresRecursosMunicipio.Select(v => v.Key))
+            //{
+            //    valoresRecursosMunicipio.TryGetValue(lblItem, out List<double> listaRecursosMunicipio);
+            //    chartData.datasets.Add(new ChartDataSet { label = lblItem, data = listaRecursosMunicipio });
+            //}
 
             return Json(JsonConvert.SerializeObject(chartData, _jsonSetting));
         }
@@ -250,6 +372,112 @@ namespace GCP_CF.Controllers
 
             // Datasets
             chartData.datasets.Add(new ChartDataSet { label = VALOR_CONTRATOS, data = valoresContratos });
+
+            return Json(JsonConvert.SerializeObject(chartData, _jsonSetting));
+        }
+
+        [HttpPost]
+        public JsonResult ValorCIADS(int? anioSeleccionado, int? idEntidadContratante, string numeroContrato, int? idEstadoContrato)
+        {
+            int anioActual = DateTime.Now.Year;
+
+            List<Contratos> contratos = db.Contratos.ToList();
+            List<PagoContrato> pagoContratos = db.PagoContrato.ToList();
+            double totalContratos = 0;
+
+            ChartData chartData = new ChartData();
+            chartData.Initialize();
+
+            if (idEntidadContratante.HasValue && idEntidadContratante.Value != -1)
+                contratos = contratos.Where(c => c.Persona_Id == idEntidadContratante).ToList();
+
+            if (!string.IsNullOrEmpty(numeroContrato))
+            {
+                int idContrato = (from cm in db.Contratos
+                                  where cm.NumeroContrato.ToUpper().Trim() == numeroContrato.ToUpper().Trim()
+                                  select cm.Contrato_Id).FirstOrDefault();
+
+                contratos = contratos.Where(c => (idContrato > 0 ?
+                            c.Contrato_Id == idContrato || (c.ContratoMarco_Id.HasValue && c.ContratoMarco_Id.Value == idContrato) : true))
+                            .ToList();
+            }
+
+            if (idEstadoContrato.HasValue && idEstadoContrato.Value != -1)
+                contratos = contratos.Where(c => c.TipoEstadoContrato_Id == idEstadoContrato).ToList();
+
+            List<double> valoresContratos = new List<double>();
+
+            int limiteAnios = 0;
+            int anioSuperior = anioActual;
+            if (!anioSeleccionado.HasValue || anioSeleccionado.Value <= 0)
+            {
+                limiteAnios = 3;
+            }
+            else
+            {
+                if (anioActual > anioSeleccionado.Value)
+                    limiteAnios = anioActual - anioSeleccionado.Value;
+                else
+                {
+                    anioSuperior = anioSeleccionado.Value;
+                    limiteAnios = 0;
+                }
+            }
+
+            if (limiteAnios > 3) limiteAnios = 3;
+            int anioInferior = anioActual - limiteAnios;
+
+            double valorContratos = 0;
+            double valorCDP = 0;
+            double valorCRP = 0;
+            double valorPagos = 0;
+
+            for (int anio = anioInferior; anio <= anioSuperior; anio++)
+            {
+                var contratosAnio = contratos.Where(c => c.FechaInicio.Year >= anio && c.FechaTerminacion.Year <= anio);
+
+                valorContratos = contratosAnio
+                    .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? c.ValorContrato : 0)
+                    .DefaultIfEmpty(0).Sum();
+                totalContratos += valorContratos;
+
+                valorCDP = contratosAnio
+                    .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? !c.ValorCDP.HasValue ? 0 : c.ValorCDP.Value : 0)
+                    .DefaultIfEmpty(0).Sum();
+                valorCDP += valorCDP;
+
+                valorCRP = contratosAnio
+                    .Select(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco ? !c.ValorCRP.HasValue ? 0 : c.ValorCRP.Value : 0)
+                    .DefaultIfEmpty(0).Sum();
+                valorCRP += valorCRP;
+
+                foreach (var contrato in contratosAnio.Where(c => c.TipoContrato != null && c.TipoContrato.Termino != tipoContratoMarco).ToList())
+                {
+                    valorPagos = pagoContratos.Where(c => c.Fecha.Year >= anio && c.Contrato_Id == contrato.Contrato_Id).Select(c => c.Valor).DefaultIfEmpty(0).Sum();
+                    valorPagos += valorPagos;
+                }
+                
+            }
+
+            valoresContratos.Add(totalContratos);
+            valoresContratos.Add(valorCDP);
+            valoresContratos.Add(valorCRP);
+            valoresContratos.Add(valorPagos);
+
+            if (anioSeleccionado < 0)
+            {
+                chartData.labels.Add("PRESUPUESTO");
+            }
+            else
+            {
+                chartData.labels.Add("PRESUPUESTO "+ anioSeleccionado.ToString());
+            }
+            chartData.labels.Add("CDP");
+            chartData.labels.Add("CRP");
+            chartData.labels.Add("PAGOS");
+
+            // Datasets
+            chartData.datasets.Add(new ChartDataSet { label = "VALOR", data = valoresContratos });
 
             return Json(JsonConvert.SerializeObject(chartData, _jsonSetting));
         }
--- GCP_CF/Controllers/HomeController.cs
+++ GCP_CF/Controllers/HomeController.cs
@@ -1,15 +1,114 @@
 ﻿using GCP_CF.Authorization;
 using GCP_CF.Helpers;
+using GCP_CF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace GCP_CF.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly GCPContext db = new GCPContext();
+
         [GCPAuthorize(Roles = RolHelper.TODOS)]
-        public ActionResult Index()
+        public ActionResult Index(FormCollection filterForm)
         {
-            return View();
+            return View(ListarContratos(filterForm));
+        }
+
+        public List<Contratos> ListarContratos(FormCollection filterForm)
+        {
+            int anio = !string.IsNullOrEmpty(filterForm["Anio"]) ? int.Parse(filterForm["Anio"]) : 0;
+            int idEntidadContratante = !string.IsNullOrEmpty(filterForm["IdEntidadContratante"]) ? int.Parse(filterForm["IdEntidadContratante"]) : 0;
+            string numeroContrato = filterForm["NumeroContrato"];
+            int idEstadoContrato = !string.IsNullOrEmpty(filterForm["IdEstadoContrato"]) ? int.Parse(filterForm["IdEstadoContrato"]) : 0;
+
+            ViewBag.IdEstadoContrato = new SelectList(db.TiposEstadoContrato.OrderBy(x => x.Descripcion), "TiposEstadoContrato_Id", "Descripcion", idEstadoContrato);
+            ViewBag.IdEntidadContratante = new SelectList(db.Personas.Where(x => x.TipoPersona_Id == 3), "Persona_Id", "NombreCompleto", idEntidadContratante);
+            ViewBag.NumeroContrato = numeroContrato;
+
+            HashSet<int> aniosContratos = new HashSet<int>();
+            var fechasContratos = db.Contratos.Select(c => new { c.FechaInicio, c.FechaTerminacion, c.FechaActaInicio, c.FechaCdp, c.FechaCrp });
+            foreach (var fechaContrato in fechasContratos)
+            {
+                aniosContratos.Add(fechaContrato.FechaInicio.Year);
+                aniosContratos.Add(fechaContrato.FechaTerminacion.Year);
+                aniosContratos.Add(fechaContrato.FechaActaInicio.Year);
+                aniosContratos.Add(fechaContrato.FechaCdp.GetValueOrDefault().Year);
+                aniosContratos.Add(fechaContrato.FechaCrp.GetValueOrDefault().Year);
+            }
+
+            ViewBag.Anio = new SelectList(aniosContratos.Distinct().Where(x => x > 0).OrderByDescending(x => x), anio);
+
+            // Guardar valores seleccionados para los gráficos
+            ViewBag.AnioSeleccionado = anio;
+            ViewBag.IdEntidadSeleccionada = idEntidadContratante;
+            ViewBag.NumeroContratoEnviado = numeroContrato;
+            ViewBag.IdEstadoSeleccionado = idEstadoContrato;
+
+            List<Contratos> list = ObtenerContratos(anio, idEntidadContratante, numeroContrato, idEstadoContrato);
+            return list != null ? list.OrderBy(x => x.Contrato_Id).ToList() : new List<Contratos>();
+        }
+
+        private List<Contratos> ObtenerContratos(int anio, int idEntidadContratante, string numeroContrato, int idEstadoContrato)
+        {
+            DateTime? fechaInicio = anio > 0 ? new DateTime(anio, 1, 1) : new DateTime?();
+            DateTime? fechaFin = anio > 0 ? new DateTime(anio, 12, 31) : new DateTime?();
+
+            bool hayFechas = fechaInicio.HasValue && fechaFin.HasValue;
+            bool hayEntidadContratante = idEntidadContratante > 0;
+            bool hayNumeroContrato = !string.IsNullOrEmpty(numeroContrato);
+            bool hayEstadoContrato = idEstadoContrato > 0;
+
+            var contratos = (from c in db.Contratos select c).Include(c => c.HistoriaObservaciones);
+
+            if (hayFechas)
+            {
+                contratos = contratos.Where(c => (fechaInicio.Value <= c.FechaInicio && c.FechaInicio <= fechaFin.Value) ||
+                                                 (fechaInicio.Value <= c.FechaTerminacion && c.FechaInicio <= fechaFin.Value) ||
+                                                 (fechaInicio.Value <= c.FechaActaInicio && c.FechaInicio <= fechaFin.Value) ||
+                                                 (fechaInicio.Value <= c.FechaCdp && c.FechaInicio <= fechaFin.Value) ||
+                                                 (fechaInicio.Value <= c.FechaCrp && c.FechaInicio <= fechaFin.Value));
+            }
+
+            if (hayEntidadContratante)
+                contratos = contratos.Where(c => c.EntidadContratante.Persona_Id == idEntidadContratante);
+
+            if (hayNumeroContrato)
+            {
+                int idContratoMarco = (from cm in db.Contratos
+                                       where cm.NumeroContrato.ToUpper().Trim() == numeroContrato.ToUpper().Trim()
+                                       select cm.Contrato_Id).FirstOrDefault();
+
+                contratos = contratos.Where(c => (idContratoMarco > 0 ? c.Contrato_Id == idContratoMarco || c.ContratoMarco_Id.Value == idContratoMarco : true))
+                                     .OrderByDescending(c => c.ContratoMarco_Id)
+                                     .OrderByDescending(c => c.NumeroContrato);
+            }
+
+            if (hayEstadoContrato)
+                contratos = contratos.Where(c => c.TipoEstadoContrato_Id == idEstadoContrato);
+
+            if (contratos != null)
+            {
+                var estados = db.TiposEstadoContrato.ToList();
+                foreach (Contratos item in contratos)
+                {
+                    foreach (var estado in estados)
+                    {
+                        if (item.TipoEstadoContrato_Id == estado.TiposEstadoContrato_Id)
+                        {
+                            item.Estado = estado.Descripcion;
+                        }
+                    }
+                }
+
+                return contratos.ToList<Contratos>();
+            }
+
+            return null;
         }
 
         [GCPAuthorize(Roles = RolHelper.TODOS)]
