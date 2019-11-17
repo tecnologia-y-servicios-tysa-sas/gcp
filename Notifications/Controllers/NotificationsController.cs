e2836188f3dd0054c1098da0a0490b80999166dd
Gustavo Salgado <tavosalgado@hotmail.com>
2019-11-17 16:32:32 -0500

    Adiciono web API Notifications

---
+++ Notifications/Controllers/NotificationsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using Notifications.Data;
+using Notifications.Models;
+
+
+namespace Notifications.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationsController : ControllerBase
+    {
+        Context context;
+        public NotificationsController(Context context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Notificacion> Get()
+        {
+
+            Context context = HttpContext.RequestServices.GetService(typeof(Context)) as Context;
+
+
+            return (context.GetAllNotifications());
+            //var empRecord = context.Notificacion.FromSqlRaw($"uspNotifications").AsNoTracking().ToList();
+            ////return View(empRecord);
+            //return null;
+        }
+    }
+}
---
+++ Notifications/Models/Notificacion.cs
@@ -0,0 +1,18 @@
+﻿using Notifications.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notifications.Models
+{
+    public class Notificacion
+    {
+        private Context context;
+
+        public string Mensaje { get; set; }
+        public string Correo { get; set; }
+    }
+}
